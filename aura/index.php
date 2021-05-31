9f21795984ce6bc850f7dff65b0187d1869adf8c
admin <sdfds@sdfdsf.ff>
2021-06-01 01:09:08 +0200

    init commit

---
+++ aura/index.php
@@ -0,0 +1,60 @@
+<?php
+require "./vendor/autoload.php";
+
+use App\QueryBuilder;
+
+$mysql = array(
+    "host" => "localhost",
+    "username" => "immersion",
+    "password" => "immersion",
+    "database" => "immersion"
+);
+
+
+    $mysql = array(
+        "host" => "localhost",
+        "username" => "immersion",
+        "password" => "immersion",
+        "database" => "immersion"
+    );
+
+    $data = array(
+        "name" => "aaa",
+        "email" => "aaa@aaa.aaa",
+        "password" => password_hash("aaa", PASSWORD_DEFAULT),
+    );
+
+    $update = array(
+        "name" => "bbb",
+        "email" => "bbb@bbb.bb",
+        "password" => password_hash("bbb", PASSWORD_DEFAULT),
+    );
+
+    function printList($query)
+{
+    $users = $query->select("users");
+    foreach ($users as $user)
+        echo "{$user['name']},  {$user['email']} <br>";
+}
+
+
+$id = null;
+$query = new QueryBuilder($mysql);
+
+echo "###### SELECT ###### <br>";
+$users = $query->select("users");
+foreach ($users as $user)
+    echo "{$user['name']},  {$user['email']} <br>";
+
+
+echo "<br><br> ###### INSERT ######<br>";
+$id = $query->insert("users", $data);
+printList($query);
+
+echo "<br><br> ###### UPDATE ######<br>";
+$query->update("users", $update, $id);
+printList($query);
+
+echo "<br><br> ###### DELETE ######<br>";
+$query->delete("users", $id);
+printList($query);
